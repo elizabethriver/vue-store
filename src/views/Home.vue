314a84ec9b6e398ac4e90fa924571174df55aeaf
Elizabeth Rivera <elizabethriver@gmail.com>
2021-06-05 16:01:05 -0500

    fetch

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,25 @@
 <template>
   <div>
-    <PxAssetsTable />
+    <PxAssetsTable v-bind:assest="assets" />
   </div>
 </template>
 
 <script>
+import api from "@/api";
+// import getAssets from "@/api";
 import PxAssetsTable from "@/components/PxAssetsTable";
 
 export default {
   name: "Home",
-  components: { PxAssetsTable }
+  components: { PxAssetsTable  },
+  data() {
+    return {
+      assets: [],
+    };
+  },
+  created() {
+    api.getAssets()
+    .then((assets) => (this.assets = assets))
+  }
 };
 </script>
